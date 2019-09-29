06de72c6b01968c605a405aeb214cb1db7db047d
Thorekt <demodraft@yahoo.com>
2019-09-29 23:13:48 +0200

    FIX

--- lister_commerce.php
+++ lister_commerce.php
@@ -7,28 +7,37 @@ $mysqli = connexion_bdd();
 $query = "SELECT * FROM commerce;";
 $result = $mysqli->query($query);
 $row = $result->fetch_all(MYSQLI_ASSOC);
+$reponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
 if($row != null){
-  $reponse["etat"] = 1;
-  $reponse["donnee"] = $row;
+  $reponse.="<etat>1</etat>";
+  $reponse.="<message>BDD OK!</message>";
+
+  $listeCommerceXML = "<listeCommerce>";
+  foreach ($row as $commerce) {
+    $listeCommerceXML .= "<commerce ";
+    foreach ($commerce as $cle => $valeur) {
+      if($cle == "id"){
+          $listeCommerceXML .= $cle.'="'.$valeur.'">';
+      }else{
+        $listeCommerceXML .= "<".$cle.">";
+        $listeCommerceXML .= $valeur;
+        $listeCommerceXML .= "</".$cle.">";
+      }
+    }
+    $listeCommerceXML .= "</commerce>";
+  }
+  $listeCommerceXML .= "</listeCommerce>";
+
+  $reponse .= $listeCommerceXML;
+
 }else {
-  $reponse["etat"] = 0;
-  $reponse["message"] = $mysqli->error;
+  $reponse.="<etat>0</etat>";
+  $reponse.="<message>".$mysqli->error."</message>";
 }
 
 $mysqli->close();
 
-$listeCommerceXML = "<listeCommerce>";
-foreach ($row as $commerce) {
-  $listeCommerceXML .= "<commerce>";
-  foreach ($commerce as $cle => $valeur) {
-      $listeCommerceXML .= "<".$cle.">";
-      $listeCommerceXML .= $valeur;
-      $listeCommerceXML .= "</".$cle.">";
-  }
-  $listeCommerceXML .= "</commerce>";
-}
-$listeCommerceXML .= "</listeCommerce>";
 
-echo $listeCommerceXML;
+echo $reponse;
 
 ?>
