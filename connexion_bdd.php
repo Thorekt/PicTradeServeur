c171aa4e98a09ea30cbf690088ab333246bb156f
Thorekt <demodraft@yahoo.com>
2019-09-29 21:08:49 +0200

    amelioration script lister_commerce.php# Please enter the commit message for your changes. Lines starting

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,15 +1,22 @@
 <?php
-define('DB_USER', "php-PicTrade"); // db user
-define('DB_PASSWORD', "PicTrade10-"); // db password (mention your db password here)
-define('DB_DATABASE', "PicTrade"); // database name
-define('DB_SERVER', "localhost"); // db server
 
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
 
-// Check connection
-if(mysqli_connect_errno()){
-echo "connection echoué: " . mysqli_connect_error();
-}else{
-	echo "connection reussie";
+function connexion_bdd(){
+	define('DB_USER', "php-PicTrade"); // db user
+	define('DB_PASSWORD', "PicTrade10-"); // db password (mention your db password here)
+	define('DB_DATABASE', "PicTrade"); // database name
+	define('DB_SERVER', "localhost"); // db server
+
+	$mysqli = new mysqli(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+	// Check connection
+	if($mysqli->connect_errno){
+		echo "connection echoué: " . $mysqli->connect_error."\n";
+		exit();
+	}else{
+		echo "connection reussie\n";
+		return	$mysqli;
+	}
 }
+
 ?>
