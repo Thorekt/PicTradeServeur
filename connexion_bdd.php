f3106005754edb2db0c85296ae178af0d2d8abe8
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-10-13 10:38:50 -0400

    recuperer commerce achever

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,15 +1,12 @@
 <?php
-
+define('DB_USER', "php-PicTrade"); // db user
+define('DB_PASSWORD', "PicTrade10-"); // db password (mention your db password here)
+define('DB_DATABASE', "PicTrade"); // database name
+define('DB_SERVER', "localhost"); // db server
 
 function connexion_bdd(){
-	define('DB_USER', "php-PicTrade"); // db user
-	define('DB_PASSWORD', "PicTrade10-"); // db password (mention your db password here)
-	define('DB_DATABASE', "PicTrade"); // database name
-	define('DB_SERVER', "localhost"); // db server
-
 	$mysqli = new mysqli(DB_SERVER,DB_USER,DB_PASSWORD,DB_DATABASE);
 
-	// Check connection
 	if($mysqli->connect_errno){
 		echo "connection echoué: " . $mysqli->connect_error."\n";
 		exit();
@@ -18,5 +15,4 @@ function connexion_bdd(){
 		return	$mysqli;
 	}
 }
-
 ?>
---
+++ recuperer_commerce.php
@@ -0,0 +1,90 @@
+<?php
+require "./connexion_bdd.php";
+
+function ajouter_commerce($placeid, $nom, $longitude, $latitude){
+  $mysqli = connexion_bdd();
+  $preparedQuery = $mysqli->prepare("INSERT INTO
+                commerce(placeID,nom,longitude,latitude) values(?,?,?,?)");
+  $preparedQuery->bind_param("ssdd",$placeid, $nom, $longitude, $latitude);
+  $preparedQuery->execute();
+  $preparedQuery->close();
+  $mysqli->close();
+}
+
+function select_avec_placeID($placeid){
+  $mysqli = connexion_bdd();
+  $preparedQuery = $mysqli->prepare("SELECT * from commerce where placeID = ?");
+  $preparedQuery->bind_param("s",$placeid);
+  $preparedQuery->execute();
+  $resultat = $preparedQuery->get_result()->fetch_all(MYSQLI_ASSOC);
+  $preparedQuery->close();
+  $mysqli->close();
+  return $resultat;
+}
+
+function select_avec_id($id){
+  $mysqli = connexion_bdd();
+  $preparedQuery = $mysqli->prepare("SELECT * from commerce where id = ?");
+  $preparedQuery->bind_param("i",$id);
+  $preparedQuery->execute();
+  $resultat = $preparedQuery->get_result()->fetch_all(MYSQLI_ASSOC);
+  $preparedQuery->close();
+  $mysqli->close();
+  return $resultat;
+}
+
+
+$mysqli = connexion_bdd();
+
+if(isset($_POST["placeID"]),isset($_POST["nom"]),
+   isset($_POST["longitude"]),isset($_POST["latitude"])){
+
+   $resultat = select_avec_placeID($_POST["placeID"]);
+   if (sizeof($resultat) == 0){
+     ajouter_commerce($_POST["placeID"], $_POST["nom"], $_POST["longitude"],
+                      $_POST["latitude"]);
+     $resultat = select_avec_placeID($_POST["placeID"]);
+
+   }
+
+
+}elseif (isset($_POST["id"])) {
+   $resultat = select_avec_id($_POST["id"]);
+}else{
+   exit();
+}
+
+
+$reponse = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+if (sizeof($resultat) == 1){
+
+
+  $reponse.="<etat>1</etat>";
+  $reponse.="<message>BDD OK!</message>";
+
+  $commerceXML .= "<commerce ";
+  foreach ($resultat[0] as $cle => $valeur) {
+    if($cle == "id"){
+      $commerceXML .= $cle.'="'.$valeur.'">';
+    }else{
+      $commerceXML .= "<".$cle.">";
+      $commerceXML .= $valeur;
+      $commerceXML .= "</".$cle.">";
+    }
+  }
+  $commerceXML .= "</commerce>";
+  $reponse .= $commerceXML;
+
+}else {
+  $reponse.="<etat>0</etat>";
+  $reponse.="<message>".$mysqli->error."</message>";
+}
+
+$reponse.="<nombreDeResultats>".sizeof($resultat)."</nombreDeResultats>";
+
+
+
+$mysqli->close();
+echo $reponse;
+
+ ?>
