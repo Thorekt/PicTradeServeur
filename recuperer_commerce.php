022caf466aec6bf54fad2629ddea285114e7bc18
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-10-16 22:12:47 -0400

    FIX

--- recuperer_commerce.php
+++ recuperer_commerce.php
@@ -4,7 +4,7 @@ require "./connexion_bdd.php";
 function ajouter_commerce($placeid, $nom, $longitude, $latitude){
   $mysqli = connexion_bdd();
   $preparedQuery = $mysqli->prepare("INSERT INTO
-                commerce(placeID,nom,longitude,latitude) values(?,?,?,?)");
+                commerce(placeID,nom_commerce,longitude,latitude) values(?,?,?,?)");
   $preparedQuery->bind_param("ssdd",$placeid, $nom, $longitude, $latitude);
   $preparedQuery->execute();
   $preparedQuery->close();
@@ -24,7 +24,7 @@ function select_avec_placeID($placeid){
 
 function select_avec_id($id){
   $mysqli = connexion_bdd();
-  $preparedQuery = $mysqli->prepare("SELECT * from commerce where id = ?");
+  $preparedQuery = $mysqli->prepare("SELECT * from commerce where id_commerce = ?");
   $preparedQuery->bind_param("i",$id);
   $preparedQuery->execute();
   $resultat = $preparedQuery->get_result()->fetch_all(MYSQLI_ASSOC);
@@ -37,14 +37,14 @@ function select_avec_id($id){
 $mysqli = connexion_bdd();
 
 if(
-  isset($_POST["placeID"],$_POST["nom"],$_POST["longitude"],$_POST["latitude"])
+  isset($_POST["placeID"],$_POST["nom_commerce"],$_POST["longitude"],$_POST["latitude"])
   )
 {
 
    $resultat = select_avec_placeID($_POST["placeID"]);
 
    if (sizeof($resultat) == 0){
-     ajouter_commerce($_POST["placeID"], $_POST["nom"], $_POST["longitude"],
+     ajouter_commerce($_POST["placeID"], $_POST["nom_commerce"], $_POST["longitude"],
                       $_POST["latitude"]);
      $resultat = select_avec_placeID($_POST["placeID"]);
 
@@ -54,6 +54,7 @@ if(
 }elseif (isset($_POST["id_commerce"])) {
    $resultat = select_avec_id($_POST["id_commerce"]);
 }else{
+    echo "<etat>0</etat>";
    exit();
 }
 
