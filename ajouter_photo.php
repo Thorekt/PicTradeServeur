3d0a269aa6758d3808a6ad6ef4393b517c4babb8
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-10-18 19:54:26 -0400

    ecriture ajouter_photo.php

---
+++ ajouter_photo.php
@@ -0,0 +1,35 @@
+<?php
+require "./connexion_bdd.php";
+
+
+if(isset($_POST["id_commerce"],$_POST["image"])){
+  $id_commerce = $_POST["id_commerce"];
+  $image = $_POST["image"];
+
+  $mysqli = connexion_bdd();
+  $preparedQuery = $mysqli->prepare("INSERT INTO
+                photo(id_commerce) values(?)");
+  $preparedQuery->bind_param("i",$id_commerce);
+  $preparedQuery->execute();
+  $preparedQuery->close();
+
+  $id_photo = $mysqli->insert_id;
+
+  if(!is_dir($id_commerce)){
+    mkdir($id_commerce, 0755, true);
+  }
+
+  $fichierPhoto = fopen($id_commerce."/".$id_photo.".txt", "w");
+  fwrite($fichierPhoto, $image);
+
+  $mysqli->close();
+  echo "ok";
+
+}else {
+  echo "echec";
+}
+
+
+
+
+?>
